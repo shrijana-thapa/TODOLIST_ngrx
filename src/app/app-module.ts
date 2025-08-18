6994a6ec46e4184190d4047e2c95d0f4a60128bd
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-08-18 11:42:13 -0700

    create store and component fortodolist

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Todolist } from './todolist_component/todolist/todolist';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Todolist
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todolist_component/todolist/todolist-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodolistRoutingModule } from './todolist-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TodolistRoutingModule
+  ]
+})
+export class TodolistModule { }
